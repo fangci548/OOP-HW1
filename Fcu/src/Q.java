72fee4f6a5aaac111765e8e42b11bf90949bc337
user <user@ED09>
2020-09-29 09:10:50 +0800

    fangci prety

--- Fcu/src/Q.java
+++ Fcu/src/Q.java
@@ -3,7 +3,7 @@ public class Q {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("This is lab haha.");
+		System.out.println("kialin stupid.");
 	}
 
 }
