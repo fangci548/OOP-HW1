0f19efd77b8cbc33169d6d63aeec60ae5deb8dd5
user <user@ED09>
2020-09-29 09:36:01 +0800

    lab1

--- 109-OOP-lab1/src/Q.java
+++ 109-OOP-lab1/src/Q.java
@@ -3,7 +3,8 @@ public class Q {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("kialin stupid.");
+		System.out.println("I am ½²¤è·O");
+		System.out.println("My nid is D0843530");
 	}
 
 }
